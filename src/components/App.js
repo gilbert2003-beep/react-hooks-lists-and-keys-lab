9e88d03cab4a2a400d9e3ff53611545fe3e5f075
gilbert.bageni <135163732+gilbert2003-beep@users.noreply.github.com>
2023-07-24 22:54:37 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
-import user from "../data/user";
+import React from 'react';
+import NavBar from './NavBar';
+import ProjectList from './ProjectList';
+import ProjectItem from './ProjectItem'; // Make sure to import ProjectItem
 
-function App() {
+const App = () => {
+  const projects = [
+    {
+      id: 1,
+      technologies: ['React', 'JavaScript', 'CSS'],
+    },
+    {
+      id: 2,
+      technologies: ['Vue', 'JavaScript', 'HTML'],
+    },
+    {
+      id: 3,
+      technologies: ['Angular', 'TypeScript', 'SCSS'],
+    },
+  ];
+
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      {/* Other components like Home and About go here */}
+      <ProjectList projects={projects} />
     </div>
   );
-}
+};
 
 export default App;
